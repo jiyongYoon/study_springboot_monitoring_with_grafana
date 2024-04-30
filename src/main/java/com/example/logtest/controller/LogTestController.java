1e3e9df8c7cc54f5fe053c6e459ebcde512e37e3
jiyong.yoon <jyyoon0615@naver.com>
2024-04-30 09:46:09 +0900

    nginx -> grafana 세팅

--- src/main/java/com/example/logtest/controller/LogTestController.java
+++ src/main/java/com/example/logtest/controller/LogTestController.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 @Slf4j
 public class LogTestController {
